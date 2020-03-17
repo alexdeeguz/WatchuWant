0ff438b1b4b97a3a1f505996d7a3d23d8cc7118f
jwchau <john.win.chau@gmail.com>
2020-03-16 23:56:46 -0700

    add login, signup, root, app components; add session actions; add reset css and specific css files for components; add frontend util and store;

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class footer extends Component {
+    render() {
+        return (
+            <div>
+                I am footer
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/users/user_show.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserShow extends Component {
+    render() {
+        return (
+            <div>
+                i am user show
+            </div>
+        )
+    }
+}
