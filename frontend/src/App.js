b1c0a837c7445e4f30ed8e3c4f3e8ca65ea6e2bc
jwchau <john.win.chau@gmail.com>
2020-03-18 14:51:56 -0700

    remove unused imports and variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,11 @@ import './App.css';
 
 //react routing
 import { AuthRoute, ProtectedRoute } from '../src/util/route_util';
-import { Switch, Route } from 'react-router-dom';
+import { Switch} from 'react-router-dom';
 
 //components
 import NavBarContainer from './components/nav/navbar_container';
 import MainPage from './components/main/main_page';
-import LoginFormContainer from './components/session/login_form_container';
-import SignupFormContainer from './components/session/signup_form_container';
 import UserShowContainer from './components/users/user_show_container';
 import Footer from './components/footer/footer';
 import Preferences from './components/preferences/preferences'
@@ -22,12 +20,7 @@ const App = () => {
     <div className='App'>
         <NavBarContainer className="nav-bar"/>
         <Switch>
-
-            <AuthRoute exact path="/" component={MainPage} />
-            {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
-          
-
-            <ProtectedRoute path='/restaurant/:id' component={RestaurantShowContainer}/>
+            <ProtectedRoute path='/restaurants/:id' component={RestaurantShowContainer}/>
             <ProtectedRoute path="/preferences" component={Preferences} />
             <ProtectedRoute path="/user" component={UserShowContainer} />
             <AuthRoute path="/" component={MainPage} />
