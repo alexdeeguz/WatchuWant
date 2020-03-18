d871b10b7c1b9cc164b72886cfa55c92c2d00a3b
alexdeeguz <alexdg316@gmail.com>
2020-03-17 18:21:05 -0700

    Remove keys_dev

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://JAKT_watchuwant:mernPro2019@watchuwant-bjm46.mongodb.net/test?retryWrites=true&w=majority",
-    secretToken: "Y;*,8sXvxa,njD*",
-    yelpAPIKey: "Yr0rjssIas6IY-hiOQ4wTqQnR6jbVpe2zqJ64O_Nin2Wx_AO545ZnSEs12uDGgcf7UPIJaz65mZc4NXl0KCt8QpDHGFjz_MQAiv7X233p4dl_mKPpLDfic4VBwpxXnYx"
-}
