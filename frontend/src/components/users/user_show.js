7d63ffa4791eceb9a544be33118da66b0b7a13bf
jwchau <john.win.chau@gmail.com>
2020-03-18 15:34:09 -0700

    create restaurant item component

--- frontend/src/components/users/user_show.js
+++ frontend/src/components/users/user_show.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import RestaurantItem from '../restauraunts/restaurant_item';
 
 export default class UserShow extends Component {
     render() {
-        return (
-            <div>
-                i am user show
-            </div>
-        )
-    }
+      return (
+        <div id='user-show'>
+          <h1>i am user show</h1>
+
+          <div className='favorites-container'>
+              <h2>Favorites</h2>
+              <div className='favorites'>
+                  <RestaurantItem />
+                  <RestaurantItem />
+                  <RestaurantItem />
+              </div>
+          </div>
+
+          <div className='visited-container'>
+              <h2>Visited</h2>
+              <div className='visited'>
+                  <RestaurantItem />
+                  <RestaurantItem />
+                  <RestaurantItem />
+              </div>
+          </div>
+
+        </div>
+      )
+  }
 }
