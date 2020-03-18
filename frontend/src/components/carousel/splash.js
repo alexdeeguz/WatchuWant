de81225f55ce354c83697b70004ce76eb705cec6
Kendra <kmodrunia@ucdavis.edu>
2020-03-18 15:32:42 -0700

    enlongated the slideshow

--- frontend/src/components/carousel/splash.js
+++ frontend/src/components/carousel/splash.js
@@ -22,7 +22,7 @@ class SplashCarousel extends React.Component{
             .fadeIn(1000)
             .end()
             .appendTo('#slideshow');
-        },  4000);
+        },  6000);
     }
 
 
@@ -34,8 +34,11 @@ class SplashCarousel extends React.Component{
                         <img className="food-slide-pic" src='https://images.unsplash.com/photo-1543352634-99a5d50ae78e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'></img>
                     </div>
                     <div>
-                        <img className="food-slide-pic" src='https://images.unsplash.com/photo-1572098873382-f8e4bf925781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'></img>
+                        <img className='food-slide-pic' src='https://images.unsplash.com/photo-1504712598893-24159a89200e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'></img>
                     </div>
+                    {/* <div>
+                        <img className="food-slide-pic" src='https://images.unsplash.com/photo-1572098873382-f8e4bf925781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'></img>
+                    </div> */}
                     <div>
                         <img className="food-slide-pic" src="https://images.unsplash.com/photo-1543352634-99a5d50ae78e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"></img>
                     </div>
@@ -48,49 +51,15 @@ class SplashCarousel extends React.Component{
                     <div>
                         <img className="food-slide-pic" src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"></img>
                     </div>
+                    
+                    <div>
+                        <img className="food-slide-pic" src="https://images.unsplash.com/photo-1541544741938-0af808871cc0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=749&q=80"></img>
+                    </div>
+                    
                 </div>
             </div>
         )
     }
-    // constructor(props){
-    //     super(props)
-    //     this.state = {
-    //         currentIdx: 0,
-    //         foodImageUrls: 
-    //     ['https://images.unsplash.com/photo-1543352634-99a5d50ae78e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
-    //     'https://images.unsplash.com/photo-1543353071-873f17a7a088?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-    //     'https://images.unsplash.com/photo-1572098873382-f8e4bf925781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
-    //     'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-    //     ]
-    //     }
-    // }
-
-    // componentDidMount(){
-    //     this.interval = setInterval(()=> this.changeBackgroundImg(), 5000);
-    // }
-
-    // changeBackgroundImg(){
-    //     let nextIdx = ((this.state.currentIdx + 1) % (this.state.foodImageUrls.length));
-    //     this.setState({currentIdx: nextIdx})
-    // }
-
-    // render(){
-    //     const { currentIdx } = this.state;
-    //     const urlString = `url('${this.state.foodImageUrls[currentIdx]})`
-    //     // debugger
-    //     return(
-    //         <div id='slideshow'>
-    //             {/* <div className='splashbackground'>
-    //                 this is random stuff
-    //             </div> */}
-    //             <div className='splashbackground' style={{ backgroundImage: urlString }}>
-    //             </div>
-    //             <div>
-    //                 contacts
-    //             </div>
-    //         </div>
-    //     )
-    // }
 
 };
 
