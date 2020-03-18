15918ba963862eaa789dc3125f87d2afd392a4bd
Kendra <kmodrunia@ucdavis.edu>
2020-03-17 18:38:38 -0700

    api k

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -12,5 +12,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://JAKT_watchuwant:mernPro2019@watchuwant-bjm46.mongodb.net/test?retryWrites=true&w=majority",
     secretToken: "Y;*,8sXvxa,njD*",
-    yelpAPIKey: "Yr0rjssIas6IY-hiOQ4wTqQnR6jbVpe2zqJ64O_Nin2Wx_AO545ZnSEs12uDGgcf7UPIJaz65mZc4NXl0KCt8QpDHGFjz_MQAiv7X233p4dl_mKPpLDfic4VBwpxXnYx"
+    yelpAPIKey: "QANNLRBgBmA4Tv0TULEE7HfafCaYMhWI_cZI0PCU2ec82NEx_ZvqKkoyth4A3I0tniedNejSylCvutvoyuF1xTKoxo9GgyQxO1niKmS8gxPUcQKNmLZlXPSVBXtxXnYx"
 }
