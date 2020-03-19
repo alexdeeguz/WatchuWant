6909de7c3eee1597cf0566da71d8db1a66c385d2
Kendra <kmodrunia@ucdavis.edu>
2020-03-19 13:57:23 -0700

    visited up to routes

---
+++ routes/api/visited.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const Visited = require('../../models/Visited');
+const validateVisitedInput = require('../../validation/visited');
+
+
+router.get("/test", (req, res) => res.json({ msg: "This is the visited route" }));
+// post
+router.post('/',
+    (req, res)=>{
+
+        const { errors, isValid } = validateVisitedInput(req.body);
+        if (!isValid){
+            return res.status(400).json(errors);   
+        }
+
+        const newVisited = new Visited({
+            restaurantId: req.body.restaurantId,
+            userId: req.body.userId,
+            imageUrl: req.body.imageUrl,
+            name: req.body.name,
+            location: req.body.location
+        })
+
+        newVisited
+            .save()
+            .then(fav => res.json(fav))
+            .catch(err=> res.status(400).json(err));
+    }
+)
+
+// router.delete('/:id',
+//     (req, res)=>{
+//         Favorite.deleteOne({_id: req.params.id})
+//             .then(()=>res.json("Restaurant removed from favorite"))
+//             .catch( err => res.status(500).json({favoritedeletion: 'Could not remove restaurant'}))
+//     }
+// )
+
+router.get('/:id',
+    (req,res)=>{
+        Visited
+            .findById(req.params.id)
+            .then(fav => res.json(fav))
+            .catch(err => res.status(400).json(err))
+    }
+)
+
+// get all
+router.get('/user/:userId',
+    (req, res)=>{
+        Visited
+            .find({userId: req.params.userId})
+            .sort( {date: -1})
+            .then(favs => res.json(favs))
+            .catch(err => res.status(400).json(err))
+    }
+)
+
+module.exports = router;
