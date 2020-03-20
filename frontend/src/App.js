1044881ad93bb0100a5e6ccb7a8837e9cc217055
jwchau <john.win.chau@gmail.com>
2020-03-20 12:15:07 -0700

    edit preference form css to match no mass navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,10 @@ import NavBarContainer from './components/nav/navbar_container';
 import UserShowContainer from './components/users/user_show_container';
 import PreferencesContainer from './components/preferences/preferences_container'
 import RestaurantContainer from './components/restaurant/restaurant_container';
+import Loading from './components/loading/spinner';
 
 // testing
-// import {Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import SplashCarousel from './components/carousel/splash';
 
 
@@ -22,6 +23,7 @@ const App = () => {
     <div className='App'>
         <NavBarContainer className="nav-bar"/>
         <Switch>
+          <ProtectedRoute path='/loading' component={Loading} />
           <ProtectedRoute path='/restaurants/:id' component={RestaurantContainer}/>
           <ProtectedRoute path="/preferences" component={PreferencesContainer} />
           <ProtectedRoute path="/user" component={UserShowContainer} />
