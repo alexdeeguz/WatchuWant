a1f30e239683e9518cb1ed7bce7971b9993220cf
Try <khovct@gmail.com>
2020-03-20 12:07:00 -0700

    modal takes up whole page

--- frontend/src/components/carousel/splash.js
+++ frontend/src/components/carousel/splash.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import "./splash.scss";
+import Modal from '../modal/modal';
 
 // const foodImages = [
 // "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=387&q=80",
