4b38a7f15f41b8650674a6f24b10ed1fbff2d2c3
Kendra <kmodrunia@ucdavis.edu>
2020-03-21 13:00:29 -0700

    alert when out of rest, tried to create pref reducer

--- frontend/src/reducers/preference_error_reducer.js
+++ frontend/src/reducers/preference_error_reducer.js
@@ -1,7 +1,7 @@
 import { RECEIVE_ERRORS } from '../actions/preference'
 
 const preferenceErrorReducer = (state = {}, action) => {
-    debugger
+    // debugger
     Object.freeze(state);
     if (action.type === RECEIVE_ERRORS ) {
         return action.errors
