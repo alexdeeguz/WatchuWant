6e765e5be698f28ff719f7c0ef6bf57f1d7b130f
Kendra <kmodrunia@ucdavis.edu>
2020-03-22 00:23:15 -0700

    increase opacity, refactored

--- frontend/src/components/carousel/banner.js
+++ frontend/src/components/carousel/banner.js
@@ -3,14 +3,12 @@ import './banner.scss'
 
 const Banner = () =>{
     return(
-        // <div className='out-most-banner-wrap'>
             <div className='banner-wrap'>
                 <div className='opaque-me'></div>
                 <div className='logo-title'>
                     WhatChu Want?
                 </div>
             </div>
-        // </div>
     )
 };
 
