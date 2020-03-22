0a07c20a89e9909941c8f34ffa196fdd67809aee
Kendra <kmodrunia@ucdavis.edu>
2020-03-21 23:19:59 -0700

    banner colors

--- frontend/src/components/carousel/banner.js
+++ frontend/src/components/carousel/banner.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './banner.scss'
 
 const Banner = () =>{
     return(
-        <div className='banner-wrap'>
-            banner
-        </div>
+        // <div className='out-most-banner-wrap'>
+            <div className='banner-wrap'>
+                <div className='opaque-me'></div>
+                <div className='logo-title'>
+                    WhatChu Want?
+                </div>
+            </div>
+        // </div>
     )
 };
 
