eb2c5d8d40673bd2460902750c69c1dfd17e0170
jwchau <john.win.chau@gmail.com>
2020-03-22 17:20:30 -0700

    add alt to icon img

--- frontend/src/components/team/person.js
+++ frontend/src/components/team/person.js
@@ -7,9 +7,9 @@ const Person = (props) => {
       <img className='photo' alt='john' src={photo}></img>
       <h2>{name}</h2>
       <div className='icons'>
-        <a href={github}><img className='icon' src='/people/github.png'></img></a>
-        <a href={linkedin}><img className='icon' src='/people/linkedin.png'></img></a>
-        <a href={angelco}><img className='icon' src='/people/angelco.png'></img></a>
+        <a href={github}><img alt='github' className='icon' src='/people/github.png'></img></a>
+        <a href={linkedin}><img alt='linkedin' className='icon' src='/people/linkedin.png'></img></a>
+        <a href={angelco}><img alt='angelco' className='icon' src='/people/angelco.png'></img></a>
       </div>
 
     </div>
